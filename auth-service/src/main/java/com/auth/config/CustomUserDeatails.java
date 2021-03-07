f3acdd485b828dc192f047b83cb724ad2655b33a
Basil Abraham <basil.abraham@turnkeyafrica.com>
2021-03-07 19:09:02 +0300

    OTG-1 Enable Authentication Using Either Email Address or Mobile Number

--- auth-service/src/main/java/com/auth/config/CustomUserDeatails.java
+++ auth-service/src/main/java/com/auth/config/CustomUserDeatails.java
@@ -24,16 +24,20 @@ public class CustomUserDeatails implements UserDetailsService {
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		//User findUserByEmail = userDao.findUserByEmail(username);
-		User findUserByMobilenumber = userDao.findUserByMobilenumber(username);
-	//	if (findUserByEmail == null) {
-	//		throw new RuntimeException("User not found..!!!");
-	//	}
-		if (findUserByMobilenumber == null) {
-			throw new RuntimeException("User not found..!!!");
+        org.springframework.security.core.userdetails.User user =null;
+                User findUserByEmail = userDao.findUserByEmail(username);
+		if (findUserByEmail == null) {
+            User findUserByMobilenumber = userDao.findUserByMobilenumber(username);
+            if (findUserByMobilenumber == null) {
+                throw new RuntimeException("User not found..!!!");
+            }
+            if(findUserByMobilenumber!=null) {
+                user = new org.springframework.security.core.userdetails.User(findUserByMobilenumber.getMobilenumber(), findUserByMobilenumber.getPassword(), true, true, true, true, findUserByMobilenumber.getRoleses());
+            }
 		}
-	//	org.springframework.security.core.userdetails.User user = new org.springframework.security.core.userdetails.User(findUserByEmail.getEmail(), findUserByEmail.getPassword(), true, true, true, true, findUserByEmail.getRoleses());
-		org.springframework.security.core.userdetails.User user = new org.springframework.security.core.userdetails.User(findUserByMobilenumber.getMobilenumber(), findUserByMobilenumber.getPassword(), true, true, true, true, findUserByMobilenumber.getRoleses());
+		if(findUserByEmail!=null) {
+            user = new org.springframework.security.core.userdetails.User(findUserByEmail.getEmail(), findUserByEmail.getPassword(), true, true, true, true, findUserByEmail.getRoleses());
+        }
 		return user;
 	}
 
